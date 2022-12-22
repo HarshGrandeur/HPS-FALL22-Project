9b4d4767e3aa442cc23e722b5ff035e83158567e
Dev <dpj7913@nyu.edu>
2022-12-22 15:22:07 -0500

    Last week's changes

--- index.php
+++ index.php
@@ -1,36 +1,25 @@
 <?php
 // Filename of your index page
 $index = "iframe.html";
-// Metadata
-$game = "Auction Triplets";
+$game = "Auction Triplets 2";
 $team = "Team Pratham";
 $instruction = <<<EOD
-<strong>Overview:</strong> <br/>
-<p> Dig That is a two-player game between a Badguy and a Detector.
-    The Badguy builds an explosive tunnel under the city and it is the role of the Detector to detect the tunnel.
-    Two people can play, or one person can play Detector and the computer will be the Badguy.</p>
-<strong>Phases:</strong> <br/>
-<ol>
-    <li> The Detector looks away from the screen while the Badguy builds a tunnel.
-        Maximum length of the tunnel is randomly generated,
-        and will be specified before building begins.
-        The tunnel needs to be a simple path with a start and end node on the top and bottom rows respectively.
-        In order to construct the tunnel, the Badguy needs to click on the edges he wants to be a part of his tunnel.
-        When he is content with the tunnel, he clicks on the "I finished my tunnel" button in the info panel and the tunnel will disappear. </li>
-    <li> Now the Detector can begin detecting by placing probes on any number of intersections or edges. When the Detector is done,
-        click the "Done placing first round of probes" button.
-        If a given probe is on top of a part of the tunnel,
-        the probe will turn green. Repeat this process up to two more times. </li>
-    <li> The Detector selects what she believes is the location of the tunnel.
-        When she is done, she clicks on the "Ready to submit final guess" button. </li>
-</ol>
-<p>If the Detector correctly detected the tunnel, her score is (intersection probes + edge probes) she used. If she incorrectly detected the tunnel, then she gets a score of infinity. Now the two players reverse roles. The winner is the player with the lowest score. </p>
+<strong>Auction Triplets</strong>
+<p> Auction Triplets is a multi-player auction game. There can be 2-8 players that can play the game. Each player satrts with a fixed initial amount of money (100). There are 4 total object types for the auction. The objective of the game is to collect 3 objects of the same type. The first player to do so wins the game.</p>
+            To play the game:<br>
+                <ul>
+                    <li>Choose the number of players from the dropdown and click start.</li>
+                    <li>The object up for auction gets highlighted and so does the player whose turn it is (the player who goes first is determined randomly).</li>
+                    <li>The player would place their bid and press `Enter`</li>
+                    <li>Then the next player bids, and the next and so on. Once all players have made their bids, the player with the highest (valid) bid wins</li>
+                    <li>The game ends when a player wins (obtains 3 objects of the same type) </li>
+                    <li>`Reset Game` can be used to reset the game</li>
+                </ul> 
 EOD;
 
 // Size of the popup window
 $width = 940;
 $height = 1000;
-// If your score is sortable, 1 if higher score is better, -1 if smaller score is better, 0 otherwise.
 $scoring = -1;
 
 include '../../template.php';
